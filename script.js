9311bc3e2323735a8b6215106a434431cc813a5c
Karankk85 <karkk8533@gmail.com>
2025-03-26 13:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const openModal = document.getElementById("openModal");
+        const modal = document.getElementById("modal");
+        const closeModal = document.getElementById("closeModal");
+        
+        openModal.addEventListener("click", () => {
+            modal.style.display = "block";
+        });
+        
+        closeModal.addEventListener("click", () => {
+            modal.style.display = "none";
+        });
+        
+        window.addEventListener("click", (e) => {
+            if (e.target === modal) {
+                modal.style.display = "none";
+            }
+        });
